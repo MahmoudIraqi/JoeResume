bc9a276a1bfc2bed612dc3cf7c30ad380e65b79f
Mahmoud el Sayed <mahmoud.elsayed@simon-kucher.com>
2023-07-23 23:00:35 +0300

    edit welcome and about page

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,35 +1,35 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 
-import { WelcomeMessageComp } from './pages/mainSide/welcomeMessage/welcome-message.components';
-import { AboutComponents } from './pages/mainSide/About/about.components';
-import { ResumeComponents } from './pages/mainSide/Resume/resume.components';
-import { WorksComponents } from './pages/mainSide/Works/works.components';
-import { ContactComponents } from './pages/mainSide/Contact/contact.components';
+import {WelcomeMessageComp} from './pages/mainSide/welcomeMessage/welcome-message.components';
+import {AboutComponents} from './pages/mainSide/About/about.components';
+import {ResumeComponents} from './pages/mainSide/Resume/resume.components';
+import {WorksComponents} from './pages/mainSide/Works/works.components';
+import {ContactComponents} from './pages/mainSide/Contact/contact.components';
 
-export const AppRoutes: Routes = [{ 
-    path: '', 
-    redirectTo: '/welcome', 
-    pathMatch: 'full' 
-},{
-    path: '',
-    component: WelcomeMessageComp
+export const AppRoutes: Routes = [{
+  path: '',
+  redirectTo: '/welcome',
+  pathMatch: 'full'
 }, {
-    path: 'welcome',
-    component: WelcomeMessageComp
+  path: '',
+  component: WelcomeMessageComp
 }, {
-    path: 'about',
-    component: AboutComponents
+  path: 'welcome',
+  component: WelcomeMessageComp
 }, {
-    path: 'resume',
-    component: ResumeComponents
+  path: 'about',
+  component: AboutComponents
 }, {
-    path: 'works',
-    component: WorksComponents
+  path: 'resume',
+  component: ResumeComponents
 }, {
-    path: 'contact',
-    component: ContactComponents
+  path: 'works',
+  component: WorksComponents
+}, {
+  path: 'contact',
+  component: ContactComponents
 }];
 
 export const routers: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
